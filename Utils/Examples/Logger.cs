c9285434b6eb8f29837693b185a0d44da03e855f
FlukeyFiddler <robertovdmeer@gmail.com>
2018-05-29 22:24:55 +0200

    moving Parsers, again....

--- Utils/Examples/Logger.cs
+++ Utils/Examples/Logger.cs
@@ -5,10 +5,11 @@ namespace nl.flukeyfiddler.bt.Utils
 {
     public static class Logger
     {
+        const string LOG_FILE_PATH = "mods\\MyMod\\log.txt";
 
-        public static void LogError(string logFilePath, Exception ex)
+        public static void LogError(Exception ex)
         {
-            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            using (StreamWriter writer = new StreamWriter(LOG_FILE_PATH, true))
             {
                 writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
                    "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
@@ -16,9 +17,9 @@ namespace nl.flukeyfiddler.bt.Utils
             }
         }
 
-        public static void LogLine(string logFilePath, string line)
+        public static void LogLine(string line)
         {
-            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            using (StreamWriter writer = new StreamWriter(LOG_FILE_PATH, true))
             {
                 writer.WriteLine(line + Environment.NewLine + "Date :" + DateTime.Now.ToString());
                 writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
