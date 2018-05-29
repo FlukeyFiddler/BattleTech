5c67e3a779d3cc1ac258fad7b6cb54a4a431f196
FlukeyFiddler <robertovdmeer@gmail.com>
2018-05-29 21:08:27 +0200

    restructuring

--- Utils/Examples/Logger.cs
+++ Utils/Examples/Logger.cs
@@ -8,7 +8,7 @@ namespace nl.flukeyfiddler.Utils
 
         public static void LogError(Exception ex)
         {
-            string filePath = "mods/NotSoPermanentEvasion/Log.txt";
+            string filePath = "mods/MyMod/Log.txt";
             using (StreamWriter writer = new StreamWriter(filePath, true))
             {
                 writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
@@ -19,7 +19,7 @@ namespace nl.flukeyfiddler.Utils
 
         public static void LogLine(String line)
         {
-            string filePath = "mods/NotSoPermanentEvasion/Log.txt";
+            string filePath = "mods/MyMod/Log.txt";
             using (StreamWriter writer = new StreamWriter(filePath, true))
             {
                 writer.WriteLine(line + Environment.NewLine + "Date :" + DateTime.Now.ToString());
