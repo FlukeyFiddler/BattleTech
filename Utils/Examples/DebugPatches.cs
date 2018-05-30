68309bf5cb30f3a7c503a942dee531d0e53c6624
FlukeyFiddler <robertovdmeer@gmail.com>
2018-05-30 17:47:53 +0200

    added Debug Patches example and copy .bat to run after build

---
+++ Utils/Examples/DebugPatches.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace nl.flukeyfiddler.bt.Utils.Examples
+{
+    [HarmonyPatch(typeof(MainMenu), "HandleEscapeKeypress")]
+    public class MainMenu_HandleEscapeKeyPress_Patch
+    {
+        public static void Postfix(MainMenu __instance)
+        {
+            Logger.LogLine("It works");
+        }
+    }
+}
