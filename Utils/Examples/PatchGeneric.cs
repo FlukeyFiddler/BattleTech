9d652cf2284559f99c255a5b886f3d4a5a43b330
FlukeyFiddler <robertovdmeer@gmail.com>
2018-05-31 18:37:09 +0200

    Added patch generic methods example

---
+++ Utils/Examples/PatchGeneric.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.Utils.Examples
+{
+    public class MyMod
+    {
+        public static void Init()
+        {
+
+            var harmony = HarmonyInstance.Create("nl.flukeyfiddler.MyMod");
+
+            var original = typeof(PilotRepresentation).GetMethod("PlayPilotVO");
+            var genericMethod = original.MakeGenericMethod(new Type[] { typeof(AudioSwitch_dialog_lines_pilots) });
+            var transpiler = typeof(PilotRepresentation_PlayPilotVO_Patch).GetMethod("Transpiler");
+            harmony.Patch(genericMethod, null, null, new HarmonyMethod(transpiler));
+
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
