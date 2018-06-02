be4f6fe7982580dfe3f50c83f80dd1f60eb23fb8
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-02 19:46:24 +0200

    Refactored LoggerUtil and added caller info

--- Utils/LoggerUtil.cs
+++ Utils/LoggerUtil.cs
@@ -1,27 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 
 namespace nl.flukeyfiddler.bt.Utils
 {
     public static class LoggerUtil
     {
-        
-        public static void LogError(LogFilePath LogFilePath, Exception ex)
+        private static readonly string endLine = 
+            Environment.NewLine + "---------------------------------------" +
+            "--------------------------------------" + Environment.NewLine;
+
+        public static void LogError(LogFilePath LogFilePath, Exception ex, MethodBase caller = null)
         {
             using (StreamWriter writer = new StreamWriter(LogFilePath.path, true))
             {
-                writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
-                   "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
-                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                writer.WriteLine(getInfoLine());
+                writer.WriteLine("Message: " + ex.Message);
+                writer.WriteLine("Stacktrace: " + ex.StackTrace);
+                writer.WriteLine(endLine);
             }
         }
 
-        public static void LogLine(LogFilePath LogFilePath, string line)
+        public static void LogLine(LogFilePath LogFilePath, string line, MethodBase caller = null)
         {
             using (StreamWriter writer = new StreamWriter(LogFilePath.path, true))
             {
-                writer.WriteLine(line + Environment.NewLine + "Date :" + DateTime.Now.ToString());
-                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                writer.WriteLine(getInfoLine());
+                writer.WriteLine(line);
+                writer.WriteLine(endLine);
             }
         }
 
@@ -29,8 +35,32 @@ namespace nl.flukeyfiddler.bt.Utils
         {
             using (StreamWriter writer = new StreamWriter(LogFilePath.path, true))
             {
-                writer.WriteLine(line + Environment.NewLine);
+                writer.WriteLine(line);
             }
+        }
+
+        public static void LogBlock(LogFilePath LogFilePath, string[] lines, MethodBase caller = null)
+        {
+            using (StreamWriter writer = new StreamWriter(LogFilePath.path, true))
+            {
+                writer.WriteLine(getInfoLine(caller));
+                
+                foreach(string line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+
+                writer.WriteLine(endLine);
+            }
+        }
+
+        private static string getInfoLine(MethodBase methodBase = null)
+        {
+            string date = DateTime.Now.ToString();
+            string methodName = (methodBase == null) ? "Not Supplied" :
+                methodBase.ReflectedType.Name + "." + methodBase.Name;
+
+            return String.Format("Date: {0}, From: {1}", date, methodName);
         }
     }
 }
