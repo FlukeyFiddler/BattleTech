9479ddc17752e995c3bd8c3368d288e6e93f09dc
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-03 14:07:38 +0200

    Add Patch Example to change description text of 1st character creation screen

--- Utils/Examples/Patches.cs
+++ Utils/Examples/Patches.cs
@@ -23,6 +23,22 @@ namespace nl.flukeyfiddler.Examples
         }
     }
 
-    
+    // Patch page 1 description of character creation
+    [HarmonyPatch(typeof(SGCharacterCreationBackgroundSelection), "ShowQuestion")]
+    public class SGCharacterCreationBackgroundSelection_ShowQuestion_Patch
+    {
+        private static void Postfix(SGCharacterCreationBackgroundSelection __instance, int idx, int ___questionIdx)
+        {
+            if(idx == 0)
+            {
+                SGCharacterCreationBackgroundSelection.BackgroundQuestion backgroundQuestion = __instance.questions[___questionIdx];
+                backgroundQuestion.description = "The Aurigan Reach is a small kingdom in the <color=#F79B26FF><b><link=TTT:52>Rimward Periphery</link></color></b>" +
+                    ", a region of space that lies at the outskirts of the more densely-colonized <color=#F79B26FF><b><link=TTT:53>Inner Sphere</link></color></b>. " +
+                    "It is home to the <color=#F79B26FF><b><link=TTT:54>Aurigan Coalition</link></color></b>, a federation organized around a parliamentary monarchy " +
+                    "and ruled by the <color=#F79B26FF><b><link=TTT:55>Arano</link></color></b> family.\r\n\r\nFor three generations, under the rule of House Arano, " +
+                    "the Aurigan Coalition has remained a relatively peaceful corner of the Periphery.\r\n\r\nIt is here your story begins...\r\n";
+            } 
+        }
+    }
 
 }
