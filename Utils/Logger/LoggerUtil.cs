08a4ea4e28d7e60940a8e9d713cd3b39c066c728
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-09 08:35:49 +0200

    Found out what the "using" keyword is for :pig:

--- Utils/Logger/LoggerUtil.cs
+++ Utils/Logger/LoggerUtil.cs
@@ -13,67 +13,74 @@ namespace nl.flukeyfiddler.bt.Utils.Logger
 
         public static void Error(LogFilePath logFilePath, Exception ex, MethodBase caller = null)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            writer.WriteLine(getInfoLineString(caller));
-            writer.WriteLine("Message: " + ex.Message);
-            writer.WriteLine("Stacktrace: " + ex.StackTrace);
-            writer.WriteLine(endLine);
+            using (StreamWriter writer = new StreamWriter(logFilePath.path))
+            {
+                writer.WriteLine(getInfoLineString(caller));
+                writer.WriteLine("Message: " + ex.Message);
+                writer.WriteLine("Stacktrace: " + ex.StackTrace);
+                writer.WriteLine(endLine);
+            }
         }
 
         public static void Line(LogFilePath logFilePath, string line, MethodBase caller = null)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            writer.WriteLine(getInfoLineString(caller));
-            writer.WriteLine(line);
-            writer.WriteLine(endLine);
-
+            using (StreamWriter writer = new StreamWriter(logFilePath.path)) {
+                writer.WriteLine(getInfoLineString(caller));
+                writer.WriteLine(line);
+                writer.WriteLine(endLine);
+            }
         }
 
         public static void InfoLine(LogFilePath logFilePath, MethodBase caller = null)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            writer.WriteLine(getInfoLineString(caller));
+            using (StreamWriter writer = new StreamWriter(logFilePath.path))
+            {
+                writer.WriteLine(getInfoLineString(caller));
+            }
         }
 
         public static void Minimal(LogFilePath logFilePath, string line)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            writer.WriteLine(line);
+            using (StreamWriter writer = new StreamWriter(logFilePath.path))
+            {
+                writer.WriteLine(line);
+            }
         }
 
         public static void EndLine(LogFilePath logFilePath)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            writer.WriteLine(endLine);
+            using (StreamWriter writer = new StreamWriter(logFilePath.path))
+            {
+                writer.WriteLine(endLine);
+            }
         }
 
         public static void Block(LogFilePath logFilePath, string[] lines, MethodBase caller = null)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            writer.WriteLine(getInfoLineString(caller));
-                
-            foreach(string line in lines)
+            using (StreamWriter writer = new StreamWriter(logFilePath.path))
             {
-                writer.WriteLine(line);
-            }
+                writer.WriteLine(getInfoLineString(caller));
 
-            writer.WriteLine(endLine);
+                foreach (string line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+
+                writer.WriteLine(endLine);
+            }
         }
 
         public static void GameStarted(LogFilePath logFilePath)
         {
-            StreamWriter writer = newStreamWriter(logFilePath);
-            for (int i = 0; i <= 3; i++)
+            using (StreamWriter writer = new StreamWriter(logFilePath.path))
+            {
+                for (int i = 0; i <= 3; i++)
                 {
                     writer.WriteLine(Regex.Replace(endLine, Environment.NewLine, ""));
                 }
                 writer.WriteLine(DateTime.Now.ToString() + ": Game started");
                 writer.WriteLine(endLine);
-        }
-
-        private static StreamWriter newStreamWriter(LogFilePath logFilePath, bool append = true)
-        {
-            return new StreamWriter(logFilePath.path);
+            }
         }
 
         private static string getInfoLineString(MethodBase methodBase = null)
