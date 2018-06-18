a453225140267ca3403a6d6fde693b37cc48170c
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-18 09:04:52 +0200

    Added an example and Fiddling

---
+++ Utils/Examples/GenericPopup.cs
@@ -0,0 +1,18 @@
+﻿using BattleTech.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nl.flukeyfiddler.bt.Utils.Examples
+{
+    class GenericPopup
+    {
+        void SomeMethod()
+        {
+            GenericPopupBuilder.Create(GenericPopupType.Info, "No saved games found").AddButton("Ok", delegate
+            {
+                this.ReceiveButtonPress("cancel");
+            }, true, null).IsNestedPopupWithBuiltInFader().Render();
+        }
+    }
+}
